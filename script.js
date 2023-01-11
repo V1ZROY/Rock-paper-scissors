4911bbf4e6500e6dcabbc0a8ec6c9264eb274072
V1ZROY <vizoronah@googlemail.com>
2023-01-11 08:02:25 +0000

    adding JS to check the winner of the game

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ console.log(choices)
 //check if choice is valid
 function playertochoose(){
     let playerchoicecheck = prompt("Make your choice: Rock, Paper or Scissors");
-    if(choices.includes(playerchoice.toLowerCase()) === false) {
+    if(choices.includes(playerchoicecheck.toLowerCase()) === false) {
     playerchoicecheck = prompt("Invalid choice: Please choose from Rock, Paper or Scissors");
     playertochoose();
     }
     return(playerchoicecheck)
 }
-let validchoice=playertochoose();
+let playerchoice=playertochoose();
 
 
 //get computer choice
@@ -26,6 +26,25 @@ let computerchoice = computertochoose()
 
 
 //Update choices
-document.getElementById("playerchoice").innerHTML = validchoice;
+document.getElementById("playerchoice").innerHTML = playerchoice;
 document.getElementById("computerchoice").innerHTML = computerchoice;
 
+
+//check winner
+function checkwinner(){
+    let result;
+    if(playerchoice === computerchoice){result = "Draw";}
+    if(playerchoice == "rock"){
+        if(computerchoice == "paper"){result="loss"}
+        if(computerchoice == "scissors"){result="win"}};
+    if(playerchoice == "paper"){
+        if(computerchoice == "scissors"){result="loss"}
+        if(computerchoice == "rock"){result="win"}};
+    if(playerchoice == "scissors"){
+        if(computerchoice == "rock"){result="loss"}
+        if(computerchoice == "paper"){result="win"}};
+    return result;
+}
+
+let result= checkwinner();
+alert(result);
