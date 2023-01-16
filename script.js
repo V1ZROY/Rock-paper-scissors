31024442749c25ec5496e1056ca516a5df6dd049
V1ZROY <vizoronah@googlemail.com>
2023-01-16 07:13:05 +0000

    fixing choice validation so that it doesn't say undefined

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
 
 //Establish choices
-let choices = ["rock", "paper", "scissors"]
+let choices = ["rock", "paper", "scissors", "r", "p", "s"]
 console.log(choices)
 
 
 //get player choice
 //check if choice is valid
 function playertochoose(){
-    let playerchoicecheck = prompt("Make your choice: Rock, Paper or Scissors");
-    if(choices.includes(playerchoicecheck.toLowerCase()) === false) {
-    playerchoicecheck = prompt("Invalid choice: Please choose from Rock, Paper or Scissors");
-    playertochoose();
+    let playerchoicecheck;
+    playerchoicecheck = prompt("Make your choice: Rock, Paper or Scissors");
+
+    if (playerchoicecheck == null || playerchoicecheck == undefined || playerchoicecheck == ""){
+        alert("Please choose from Rock, Paper or Scissors");
+        playerchoicecheck = playertochoose();
     }
-    return(playerchoicecheck)
+
+    if (choices.includes(playerchoicecheck.toLowerCase() == false)) {
+        alert("Invalid choice: Please choose from Rock, Paper or Scissors");
+        playerchoicecheck = playertochoose();
+    } 
+    
+    else {
+    return playerchoicecheck.toLowerCase().substring(0,1)
+    }
 }
-let playerchoice=playertochoose();
+
+let playerchoice = playertochoose();
 
 
 //get computer choice
 function computertochoose(){
     let computerchoice = choices[Math.floor(Math.random() * 3)]
-    return computerchoice
+    return computerchoice.substring(0,1)
 }
 let computerchoice = computertochoose()
 
 
-//Update choices
-document.getElementById("playerchoice").innerHTML = playerchoice;
-document.getElementById("computerchoice").innerHTML = computerchoice;
+//Display capitalised choice as full word
+function displaychoice(choice){
+    switch(choice){
+        case 'r':
+            return "Rock";
+        case 'p':
+            return "Paper";
+        case 's':
+            return "Scissors";    
+    }
+}
+
+//Update choices on page
+document.getElementById("playerchoice").innerHTML = displaychoice(playerchoice);
+document.getElementById("computerchoice").innerHTML = displaychoice(computerchoice);
 
 
 //check winner
 function checkwinner(){
     let result;
+
     if(playerchoice === computerchoice){result = "Draw";}
-    if(playerchoice == "rock"){
-        if(computerchoice == "paper"){result="loss"}
-        if(computerchoice == "scissors"){result="win"}};
-    if(playerchoice == "paper"){
-        if(computerchoice == "scissors"){result="loss"}
-        if(computerchoice == "rock"){result="win"}};
-    if(playerchoice == "scissors"){
-        if(computerchoice == "rock"){result="loss"}
-        if(computerchoice == "paper"){result="win"}};
+
+    if(playerchoice == "r"){
+        if(computerchoice == "p"){result="loss"}
+        if(computerchoice == "s"){result="win"}};
+
+    if(playerchoice == "p"){
+        if(computerchoice == "s"){result="loss"}
+        if(computerchoice == "r"){result="win"}};
+
+    if(playerchoice == "s"){
+        if(computerchoice == "r"){result="loss"}
+        if(computerchoice == "p"){result="win"}};
+        
     return result;
 }
 
